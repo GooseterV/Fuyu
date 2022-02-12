6a8157b53b3cff997df5f7ec6a3fa3c5471a4ba1
GooseterV <goosevincent@gmail.com>
2022-02-11 20:59:33 -0500

    cooking updates

--- cooking.py
+++ cooking.py
@@ -1,45 +1,23 @@
 import os
+import cloudinary
+import cloudinary.api
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
+cloudinary.config(
+	cloud_name = "gooseterv",
+	api_key = os.getenv("CLOUDINARY_API_KEY"),
+	api_secret = os.getenv("CLOUDINARY_API_SECRET"),
+	secure = True
+)
 @app.route('/upload')
 def index():
-
-	return r"""
-    <style>
-	    @import url('https://fonts.googleapis.com/css2?family=Baloo+Bhai+2&family=Noto+Sans+JP&family=Red+Hat+Display:wght@300&display=swap');
-	    @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100&family=Montserrat:wght@300;500&display=swap');
-	    * {
-		    font-family:"Montserrat";
-	    }
-     
-        #upload_widget {
-            position:absolute;
-            top:50%;
-            left:50%;
-            transform:translate(-50%, -50%);
-        }
-    </style>
-
-    <button id="upload_widget" class="cloudinary-button">Upload files</button>
-
-    <script src="https://upload-widget.cloudinary.com/global/all.js" type="text/javascript"></script>  
-
-    <script type="text/javascript">  
-        var myWidget = cloudinary.createUploadWidget({
-	        cloudName: 'gooseterv', 
-	        uploadPreset: 'lt62f4ih'}, (error, result) => { 
-	    if (!error && result && result.event === "success") { 
-		    console.log('Done! Here is the image info: ', result.info); 
-	    }
-    })
-
-    document.getElementById("upload_widget").addEventListener("click", function(){
-	    myWidget.open();
-    }, false);
-    </script>
-    """
+	galleryUrlHtml = "<pre>"
+	for resource in cloudinary.api.resources(type="upload", prefix="cooking", max_results=500)["resources"]:
+		galleryUrlHtml += f"<a href=\"{resource['secure_url']}\"> {resource['public_id']}.{resource['format']} </a>\n"
+	galleryUrlHtml += "</pre>"
+	print(galleryUrlHtml)
+	return open("cooking.html").read().replace("\"{0}\"", os.getenv("CLOUDINARY_API_KEY")).replace("\"{1}\"", galleryUrlHtml)
 
 
 if __name__ == "__main__":
